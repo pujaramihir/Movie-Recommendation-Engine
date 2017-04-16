117f766179d20efc8f5d8548362768da5691a588
pujaramihir <mihirppujara@gmail.com>
2017-04-16 13:53:47 -0400

    item item collaborative filtering

--- InsertRatingsToMongoDB.py
+++ InsertRatingsToMongoDB.py
@@ -13,6 +13,7 @@ spark = SparkSession \
 data = sc.textFile("./Dataset/ml-latest/ratings.csv")
 ratings = data.map(lambda l: l.split(",")).map(lambda l: (int(l[0]), int(l[1]), float(l[2])))
 ratingsPartitioned = ratings.partitionBy(24)
+
 ratings = spark.createDataFrame(ratingsPartitioned, ["userId","movieId","rating"])
 ratings.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").option("database","movielens").option("collection", "ratings1").save()
 ratings.show()    
